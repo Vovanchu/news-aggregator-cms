280bd085b7a71782b6e5c59d9ecd466a7ad07cbe
Vouchyk Vova <nechayvova2005@gmail.com>
2026-02-07 17:20:17 +0000

    Add middlewares config updates

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,23 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "http://localhost:5173",
+        "https://vovanchu.github.io/news-aggregator/",
+      ],
+    },
+  },
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
